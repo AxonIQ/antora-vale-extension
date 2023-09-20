8358c9411a9cb0813658c92112c2487c08eec0ae
Milen Dyankov <milendyankov@gmail.com>
2023-09-20 14:20:56 +0200

    Search for and replace redirect messages

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-module.exports.register = function ({ config }) {
-    const execSync = require('child_process').execSync;
-    const { valeConfig, updateStyles } = config
-    const logger = this.getLogger('vale-extension')
+const replaceInFiles = require('replace-in-files');
+const execSync = require('child_process').execSync;
 
-    this.on('sitePublished', () => {
+class ValeExtension {
+  static register ({ config }) {
+    return new ValeExtension(this, config)
+  }
 
-        var command = "vale --no-wrap --sort build";
-        var syncCommand = "vale sync";
-        if (valeConfig) {
-            command = `vale --no-wrap --sort --config ${valeConfig} build`;
-            syncCommand = `vale --config ${valeConfig} sync`;
-        }
+  constructor (context, config) {
+    ;(this.context = context)
+      .on('sitePublished', this.onSitePublished.bind(this, context))
+      this.valeConfig = config.valeConfig
+      this.updateStyles = config.updateStyles
+      this.logger = context.getLogger('vale-extension')
+    }
 
-        if (updateStyles) {
-            logger.info("updating external styles");
-            try {
-                execSync(syncCommand, { stdio: 'inherit' });
-            } catch (err) {
-                logger.debug(err);
-                this.stop(1);
-            }
-        }
-        logger.info("checking for writing style issues...");
-        logger.debug(`running "${command}"`);
-        var time = new Date();
+  async onSitePublished (context) {
+    const options = {
+      files: 'build/site/**/*.html',
+      from: 'The page you requested has been relocated',
+      to: 'The page you requested has new location',
+    };
+    var time = new Date();
+    try {
+      this.logger.info("rephrasing redirect messages...");
+      const data = await replaceInFiles(options);
+    } catch (error) {
+      this.logger.debug(err);
+      context.stop(1);
+    } finally {
+      time = (new Date() - time) / 1000
+      this.logger.info("rephrasing redirect messages completed in %s seconds", time);
+    }
+
+    var command = "vale --no-wrap --sort build";
+    var syncCommand = "vale sync";
+    if (this.valeConfig) {
+        command = `vale --no-wrap --sort --config ${this.valeConfig} build`;
+        syncCommand = `vale --config ${this.valeConfig} sync`;
+    }
+
+    if (this.updateStyles) {
+        this.logger.info("updating external styles");
         try {
-            execSync(command, { stdio: 'inherit' });
+            execSync(syncCommand, { stdio: 'inherit' });
         } catch (err) {
-            logger.debug(err);
+            this.logger.debug(err);
             this.stop(1);
-        } finally {
-            time = (new Date() - time) / 1000
-            logger.info("writing style check completed in %s seconds", time);
         }
-    })
+    }
+    this.logger.info("checking for writing style issues...");
+    this.logger.debug(`running "${command}"`);
+    var time = new Date();
+    try {
+        execSync(command, { stdio: 'inherit' });
+    } catch (err) {
+        this.logger.debug(err);
+        context.stop(1);
+    } finally {
+        time = (new Date() - time) / 1000
+        this.logger.info("writing style check completed in %s seconds", time);
+    }
+
+  }
 }
 
-
+module.exports = ValeExtension
